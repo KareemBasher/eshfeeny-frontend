0d550b33d63723a5fe87c139ab6d08739393b9d5
Karim.basher <kareem.basher@gmail.com>
2023-02-04 22:08:51 +0200

    Installed prettier and eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    semi: ['error', 'never'],
+    'no-use-before-define': ['error', { functions: true, classes: true }],
+    'prettier/prettier': 2, // Means error
+    'no-console': 0, // Means warning
+    'no-var': 'error',
+    'prefer-const': 'error'
+  }
+}
