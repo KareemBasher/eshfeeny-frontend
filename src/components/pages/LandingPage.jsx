c972bf80c2a7bb3cced111e1304aed922518e66d
kareembasher <kareem.basher@gmail.com>
2023-02-11 17:11:53 +0200

    Used max-width instead of just width.

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div className=" flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <img className="w-[306px] my-14" src={logo} alt="logo" />
       <p className="text-[28px]">مرحبا بك في تطبيق أشفينى</p>
 
-      <div className="flex flex-col mt-32">
+      <div className="w-[472px] flex flex-col mt-32">
         <Link to="/login">
           <WideButton content={'تسجيل الدخول'} />
         </Link>
