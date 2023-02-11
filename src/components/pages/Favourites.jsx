9bc98fd1258ec4254aabb228c8f0b063969fde3c
Abdalla <bodi.tarek.bt@gmail.com>
2023-02-11 20:36:51 +0200

    Fixed some visuals

--- src/components/pages/Favourites.jsx
+++ src/components/pages/Favourites.jsx
@@ -3,11 +3,11 @@ import SearchBar from '../common/SearchBar'
 import RoundButton from '../common/RoundButton'
 import NavList from '../common/NavList'
 import HeartDark from '../../assets/common/HeartDark.svg'
+import HeartLight from '../../assets/common/HeartLight.svg'
 import Location from '../../assets/common/Location.svg'
 import Cart from '../../assets/common/Cart.svg'
 import Person from '../../assets/common/Person.svg'
 import PageEmpty from '../common/PageEmpty'
-import GroupHeart from '../../assets/common/GroupHeart.svg'
 
 const Favourites = () => {
   const [query, setQuery] = useState('')
@@ -17,18 +17,18 @@ const Favourites = () => {
 
   return (
     <>
-      <div className="flex">
+      <div className="flex pt-3 px-[112px]">
         <SearchBar onGetData={searchResult} />
         <RoundButton onGetLogo={HeartDark} onGetText="المفضلة" onGetPath="/favorites" />
         <RoundButton onGetLogo={Location} onGetText="أقرب صيدلية" onGetPath="/location" />
         <RoundButton onGetLogo={Cart} onGetText="العربة" onGetPath="/cart" />
         <RoundButton onGetLogo={Person} onGetText="حسابي" onGetPath="/profile" />
       </div>
-      <div className=" my-5 border  w-full  py-3  ">
+      <div className="my-5 py-3 border w-full">
         <NavList />
       </div>
       <div>
-        <PageEmpty onGetLogo={GroupHeart} onGetText="لا توجد أي منتجات مفضلة لديك "></PageEmpty>
+        <PageEmpty onGetLogo={HeartLight} onGetText="لا توجد أي منتجات مفضلة لديك"></PageEmpty>
       </div>
     </>
   )
