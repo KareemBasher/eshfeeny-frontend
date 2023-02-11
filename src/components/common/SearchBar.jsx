aebd5ebb6bc9dc6bd0cb3c936913cf3f614d16ce
Abdalla <bodi.tarek.bt@gmail.com>
2023-02-11 02:45:21 +0200

    Refactored code after using routes

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -3,9 +3,9 @@ import SearchLogo from '../../assets/common/SearchIcon.svg'
 
 const SearchBar = ({ onGetData }) => {
   return (
-      <div className='flex justify-end bg-[#F7F7F7] rounded-full w-auto shadow-md'>
-          <input className='bg-[#F7F7F7] outline-none w-auto' dir='rtl' type='text' placeholder='ما الذي تبحت عنه؟' onChange={onGetData} />
+      <div className='flex justify-start bg-[#F7F7F7] rounded-full w-96 shadow-md'>
         <img className='px-5 py-3' src={SearchLogo} alt='Search Logo' />
+        <input className='bg-[#F7F7F7] outline-none w-auto' dir='rtl' type='text' placeholder='ما الذي تبحت عنه؟' onChange={onGetData} />
       </div>
   )
 }
