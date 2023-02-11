48daf019cd638454d542f6c510c435233903a5e5
kareembasher <kareem.basher@gmail.com>
2023-02-11 02:16:23 +0200

    Finished landing page.

---
+++ src/components/pages/landingPage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../../../assets/common/logo.svg'
+import WideButton from './WideButton'
+
+const index = () => {
+  return (
+    <div className=" flex flex-col items-center justify-center font-cairo">
+      <img className="w-[306px] my-14" src={logo} alt="logo" />
+      <p className="text-[28px]">مرحبا بك في تطبيق أشفينى</p>
+
+      <div className="flex flex-col mt-32">
+        <WideButton content={'تسجيل الدخول'} />
+        <WideButton content={'إنشاء حساب'} />
+      </div>
+    </div>
+  )
+}
+
+export default index
