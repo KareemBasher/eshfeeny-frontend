0bafd21365e5c5f28e05b6d2cc40fdc59812c85b
Abdalla <bodi.tarek.bt@gmail.com>
2023-02-11 03:58:36 +0200

    Added Round Buttons using <Link>, Fixed resizing problems

--- src/components/pages/Favourites.jsx
+++ src/components/pages/Favourites.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
 import SearchBar from '../common/SearchBar'
 import RoundButton from '../common/RoundButton'
+import HeartDark from '../../assets/common/HeartDark.svg'
+import Location from '../../assets/common/Location.svg'
+import Cart from '../../assets/common/Cart.svg'
+import Person from '../../assets/common/Person.svg'
 
 const Favourites = () => {
-    const [query, setQuery] = useState('')
-    const searchResult = (e) => {
-        setQuery(e.target.value)
-    }
+  const [query, setQuery] = useState('')
+  const searchResult = (e) => {
+    setQuery(e.target.value)
+  }
 
   return (
-    <div className='flex'>
-        <SearchBar onGetData={searchResult} />
-        <RoundButton />
+    <div className="flex">
+      <SearchBar onGetData={searchResult} />
+      <RoundButton onGetLogo={HeartDark} onGetText="المفضلة" />
+      <RoundButton onGetLogo={Location} onGetText="أقرب صيدلية" />
+      <RoundButton onGetLogo={Cart} onGetText="العربة" />
+      <RoundButton onGetLogo={Person} onGetText="حسابي" />
     </div>
   )
 }
