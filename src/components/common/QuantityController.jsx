e38ee89cb639c2efcc57fd594484dd05b0849ba0
Abdalla <bodi.tarek.bt@gmail.com>
2023-02-13 22:35:21 +0200

    Quantity Controler now shows the quantity correctly, and only when requested ( no wasted resources )

--- src/components/common/QuantityController.jsx
+++ src/components/common/QuantityController.jsx
@@ -6,16 +6,23 @@ import RemoveButton from '../../assets/common/DeleteButton.svg'
 /*     API      */
 import * as ProductsAPI from '../../utils/productsAPI'
 
-const QuantityController = ({ handleHideComponent }) => {
+const QuantityController = ({ handleHideComponent, onGetUserID, onGetProductID }) => {
+  const [quantity, seQyantity] = useState('0')
+  useEffect(() => {
+    const itemQuantity = async () => {
+      seQyantity(await ProductsAPI.checkCart(onGetUserID, onGetProductID))
+    }
+    itemQuantity()
+  }, [])
   return (
     <div className=" flex flex-row">
       <button>
         <img src={IncrementButton} className="w-[48px] h-[45px] box-border  " />
       </button>
       <p className="text-center text-[#0597F2] text-[28px] rounded-[10px] w-[156px] h-[45px] m-3 bg-[#c4e3f7]">
-        {onGetQuantity}
+        {quantity}
       </p>
-      {onGetQuantity > 1 ? (
+      {quantity > 1 ? (
         <button>
           <img src={DecrementButton} className="w-[48px] h-[45px] box-border " />
         </button>
