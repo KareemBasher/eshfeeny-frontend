26a670b063798a3a9923f6d610c231c1bbace12e
Abdalla <bodi.tarek.bt@gmail.com>
2023-02-16 17:51:55 +0200

    send to krkr

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 /*    Components    */
 import UserNavigation from '../common/UserNavigation'
@@ -8,6 +8,15 @@ import * as ProductsAPI from '../../utils/productsAPI' //getProduct
 const ProductPage = () => {
   const params = useParams()
   const productID = useState(params.id)
+
+//   useEffect(() => {
+//     const getProduct = async () => {
+//       console.log(params.id)
+//       console.log(await ProductsAPI.getProduct(params.id))
+//     }
+//     getProduct()
+//   })
+
   return (
     <div>
       <UserNavigation />
