c3a706fbd29089494cb76aae7ebed90b60b57b84
Abdalla <bodi.tarek.bt@gmail.com>
2023-02-16 17:33:23 +0200

    created Products Page

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+/*    Components    */
+import UserNavigation from '../common/UserNavigation'
 /*       API       */
 import * as ProductsAPI from '../../utils/productsAPI' //getProduct
 
 const ProductPage = () => {
+  const params = useParams()
+  const productID = useState(params.id)
   return (
     <div>
-        
+      <UserNavigation />
     </div>
   )
 }
