7ef22a0dbb43512fe532f0e17ab2dbf68f0dc09f
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-17 17:39:59 +0200

    created Main.jsx will containe image slider

---
+++ src/components/pages/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserNavigation from '../../common/UserNavigation'
+const Main = () => {
+  return (
+    <div>
+      <UserNavigation />
+    </div>
+  )
+}
+
+export default Main
