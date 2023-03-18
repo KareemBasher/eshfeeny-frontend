1d14adfd2dd012e5ddad849359d32490aa3f0ef3
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-18 22:01:09 +0200

    put onClock on the div that contains the arrows

--- src/components/pages/Main/ImageSlider.jsx
+++ src/components/pages/Main/ImageSlider.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+/*       Icons       */
 import RightArrow from '../../../assets/MainPage/RightArrow.svg'
 import LeftArrow from '../../../assets/MainPage/LeftArrow.svg'
 import Image_1 from '../../../assets/MainPage/Image_1.svg'
@@ -6,8 +7,9 @@ import Image_2 from '../../../assets/MainPage/Image_2.svg'
 import Image_3 from '../../../assets/MainPage/Image_3.svg'
 import Image_4 from '../../../assets/MainPage/Image_4.svg'
 import Image_5 from '../../../assets/MainPage/Image_5.svg'
-import './Dot.css'
 import { FaCircle } from 'react-icons/fa'
+/*       css       */
+import './Dot.css'
 
 const ImageSlider = () => {
   const images = [
@@ -55,12 +57,18 @@ const ImageSlider = () => {
       <div className="w-full mx-36 py-10 relative flex justify-center">
         <img className="w-full rounded-2xl" src={images[currentIndex].url} alt="" />
         {/* Left Arrow */}
-        <div className="h-[44px] w-[44px] flex justify-center items-center bg-[#f5f5f581] hover:opacity-60 rounded-full absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl p-2 cursor-pointer mx-5">
-          <img src={LeftArrow} onClick={prevSlide} />
+        <div
+          onClick={prevSlide}
+          className="h-[44px] w-[44px] flex justify-center items-center bg-[#f5f5f581] hover:opacity-60 rounded-full absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl p-2 cursor-pointer mx-5"
+        >
+          <img src={LeftArrow} />
         </div>
         {/* Right Arrow */}
-        <div className="h-[44px] w-[44px] flex justify-center items-center bg-[#f5f5f581] hover:opacity-60 rounded-full absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl p-2 cursor-pointer mx-5">
-          <img src={RightArrow} onClick={nextSlide} />
+        <div
+          onClick={nextSlide}
+          className="h-[44px] w-[44px] flex justify-center items-center bg-[#f5f5f581] hover:opacity-60 rounded-full absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl p-2 cursor-pointer mx-5"
+        >
+          <img src={RightArrow} />
         </div>
         <div className="absolute flex bottom-[10%] right-[45%]" dir="ltr">
           {images.map((slide, slideIndex) => (
