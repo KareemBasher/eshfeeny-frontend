e4f99eadbad8aa7e2588f068b32abc114bc9ce86
Karim.basher <kareem.basher@gmail.com>
2023-03-18 22:51:58 +0200

    Changed quantity controller style.

--- src/components/common/QuantityController.jsx
+++ src/components/common/QuantityController.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 /*      Icons      */
 import IncrementButton from '../../assets/common/AddButton.svg'
 import DecrementButton from '../../assets/common/DecrementButton.svg'
-import RemoveButton from '../../assets/common/DeleteButton.svg'
 /*     API      */
 import * as ProductsAPI from '../../utils/productsAPI'
 import * as UsersAPI from '../../utils/usersAPI'
@@ -10,6 +9,7 @@ import * as UsersAPI from '../../utils/usersAPI'
 const QuantityController = ({ handleHideComponent, onGetUserID, onGetProductID }) => {
   const [baseQuantity, setBaseQuantity] = useState('0') //value in data base
   const [modifiedQuantity, setModifiedQuantity] = useState(0) //shown value
+
   useEffect(() => {
     const itemQuantity = async () => {
       const checkQuantity = await ProductsAPI.checkCart(onGetUserID, onGetProductID)
@@ -25,22 +25,26 @@ const QuantityController = ({ handleHideComponent, onGetUserID, onGetProductID }
     }
     itemQuantity()
   }, [])
+
   const increment = async (userID, productID) => {
     setModifiedQuantity(modifiedQuantity + 1)
     await UsersAPI.incrementQuantity(userID, productID)
     setBaseQuantity(baseQuantity + 1)
   }
+
   const decrement = async (userID, productID) => {
     setModifiedQuantity(modifiedQuantity - 1)
     await UsersAPI.decrementQuantity(userID, productID)
     setBaseQuantity(baseQuantity - 1)
   }
+
   const removeFromCart = async (userID, productID) => {
     handleHideComponent()
     await UsersAPI.removeFromCart(userID, productID)
   }
+
   return (
-    <div className="flex flex-row justify-center">
+    <div className="flex flex-row justify-center mb-3">
       <button>
         <img
           src={IncrementButton}
@@ -49,23 +53,21 @@ const QuantityController = ({ handleHideComponent, onGetUserID, onGetProductID }
           alt="IncrementButton"
         />
       </button>
-      <p className="flex items-center justify-center text-blue text-[20px] rounded-[10px] w-[100px] h-[35px] m-1 bg-[#D4EEFF]">
+      <p className="flex items-center justify-center text-blue text-[20px] rounded-[10px] w-[100px] h-[35px] m-1 bg-[#CCE6FF]">
         {modifiedQuantity}
       </p>
-      {modifiedQuantity > 1 ? (
-        <button>
-          <img
-            src={DecrementButton}
-            className="w-[40px] h-[35px] box-border "
-            onClick={() => decrement(onGetUserID, onGetProductID)}
-            alt="DecrementButton"
-          />
-        </button>
-      ) : (
-        <button onClick={() => removeFromCart(onGetUserID, onGetProductID)}>
-          <img src={RemoveButton} className="w-[40px] h-[35px] box-border" alt="RemoveFromCart" />
-        </button>
-      )}
+      <button>
+        <img
+          src={DecrementButton}
+          className="w-[40px] h-[35px] box-border "
+          onClick={() =>
+            modifiedQuantity > 1
+              ? decrement(onGetUserID, onGetProductID)
+              : removeFromCart(onGetUserID, onGetProductID)
+          }
+          alt="DecrementButton"
+        />
+      </button>
     </div>
   )
 }
