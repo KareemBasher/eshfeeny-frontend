64ef35f97060f974cff821b65a45fd3f2a485711
Abdalla <bodi.tarek.bt@gmail.com>
2023-03-18 19:28:11 +0200

    Logo now navigates to home page

--- src/components/common/LogoScript.jsx
+++ src/components/common/LogoScript.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/common/Logo.svg'
 
 const LogoScript = () => {
   return (
-    <div className="flex justify-center items-center">
+    <Link to="/home" className="flex justify-center items-center">
       <img src={Logo} className="w-[40px] h-[40px] mx-5" alt="Logo" />
       <p className="text-[20px] ml-10">أشفيني</p>
-    </div>
+    </Link>
   )
 }
 
