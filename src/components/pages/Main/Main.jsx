9c2b07a6cbef648a73da580cc2c538f35326601b
Abdalla <bodi.tarek.bt@gmail.com>
2023-03-18 22:44:43 +0200

    Created category items component

--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+/*        Components        */
 import UserNavigation from '../../common/UserNavigation'
 import ImageSlider from './ImageSlider'
+import CatigoryItems from './CatigoryItems'
 
 const Main = ({ loggedInUser }) => {
   return (
     <div>
       <UserNavigation loggedInUser={loggedInUser} />
       <ImageSlider />
+      <CatigoryItems onGetTitle="الكحة" loggedInUser={loggedInUser} />
+      <CatigoryItems onGetTitle="المغص" loggedInUser={loggedInUser} />
+      <CatigoryItems onGetTitle="امساك" loggedInUser={loggedInUser} />
     </div>
   )
 }
