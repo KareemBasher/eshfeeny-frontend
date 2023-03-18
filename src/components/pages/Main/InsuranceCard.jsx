739b28bfae1938e026b7b549b0f1605f904b2df7
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-18 19:42:42 +0200

    created insuranceCard component

---
+++ src/components/pages/Main/InsuranceCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import InsuranceImage from '../../../assets/MainPage/InsuranceImage.svg'
+
+const InsuranceCard = () => {
+  return <div>InsuranceCard</div>
+}
+
+export default InsuranceCard
