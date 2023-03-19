6af25b93c6c47c4e6f398fa114a8c81711f0c8d4
Karim.basher <kareem.basher@gmail.com>
2023-03-19 23:24:26 +0200

    Finished signup page functionality.

--- src/components/pages/signUp/SignUp.jsx
+++ src/components/pages/signUp/SignUp.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CredentialsInput from '../../common/CredentialsInput'
 import WelcomeLogo from '../../common/WelcomeLogo'
 import WideButton from '../../common/WideButton'
 import AlternateSignin from '../../common/AlternateSignin'
 import SingupVector from '../../../assets/common/SingupVector.svg'
-const SignUp = () => {
+import { createUser } from '../../../utils/usersAPI'
+
+const SignUp = ({ changeLoggedUser }) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -18,22 +20,57 @@ const SignUp = () => {
     else if (name === 'checkPassword') setCheckPassowrd(value)
   }
 
-  const handleSubmit = () => {
+  const inputValidation = () => {
     if (name.length === 0) setError((prev) => ({ ...prev, nameLength: true }))
     else setError((prev) => ({ ...prev, nameLength: false }))
 
     if (email.length === 0) setError((prev) => ({ ...prev, emailLength: true }))
     else setError((prev) => ({ ...prev, emailLength: false }))
 
-    if (!email.match('/^S+@S+.S+$/')) setError((prev) => ({ ...prev, emailCheck: true }))
+    if (!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))
+      setError((prev) => ({ ...prev, emailCheck: true }))
     else setError((prev) => ({ ...prev, emailCheck: false }))
 
     if (password.length < 8) setError((prev) => ({ ...prev, passwordLength: true }))
     else setError((prev) => ({ ...prev, passwordLength: false }))
 
-    if (password === checkPassword) setError((prev) => ({ ...prev, passwordCheck: true }))
-    else setError((prev) => ({ ...prev, passwordCheck: true }))
+    if (!password === checkPassword) setError((prev) => ({ ...prev, passwordCheck: true }))
+    else setError((prev) => ({ ...prev, passwordCheck: false }))
   }
+
+  const handleSubmit = async () => {
+    inputValidation()
+
+    if (
+      !error.nameLength &&
+      !error.emailLength &&
+      !error.emailCheck &&
+      !error.passwordLength &&
+      !error.passwordCheck
+    ) {
+      const result = await createUser(name, email, password)
+
+      if (result === 'User already exists') setError((prev) => ({ ...prev, userExists: true }))
+      else {
+        setError((prev) => ({ ...prev, userExists: false }))
+        changeLoggedUser(result._id)
+        window.location.href = '/home'
+      }
+    }
+  }
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      if (event.key === 'Enter') {
+        handleSubmit()
+      }
+    }
+    window.addEventListener('keydown', handleKeydown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown)
+    }
+  }, [password])
 
   return (
     <>
@@ -41,7 +78,7 @@ const SignUp = () => {
         <WelcomeLogo />
       </div>
 
-      <div className="flex justify-evenly ml-24">
+      <div className="flex h-[75vh] justify-around">
         <div className="flex flex-col h-[75vh]">
           <div className=" w-[472px] flex flex-col">
             <p className="text-[28px] mb-10 text-right">إنشاء حساب جديد</p>
@@ -71,10 +108,12 @@ const SignUp = () => {
                 handleInput={handleInput}
               />
               {error.emailLength ? (
-                <span className="text-[#EB1D36] text-[14px]">من فضلك ادخل البريد</span>
+                <span className="text-[#EB1D36] text-[14px]">من فضلك ادخل بريدك الالكتروني</span>
+              ) : error.emailCheck ? (
+                <span className="text-[#EB1D36] text-[14px]">البريد الاكتروني غير صحيح</span>
               ) : (
-                error.emailCheck && (
-                  <span className="text-[#EB1D36] text-[14px]">البريد الاكتروني غير صحيح</span>
+                error.userExists && (
+                  <span className="text-[#EB1D36] text-[14px]">البريد الاكتروني مسجل بالفعل</span>
                 )
               )}
             </div>
@@ -103,14 +142,12 @@ const SignUp = () => {
                 handleInput={handleInput}
               />
               {error.passwordCheck && (
-                <span className="text-[#EB1D36] text-[14px] mb-10">
-                  من فضلك ادخل كلمه مرور صحيحة
-                </span>
+                <span className="text-[#EB1D36] text-[14px]">من فضلك ادخل نفس كلمه المرور</span>
               )}
             </div>
           </div>
 
-          <div className="flex justify-start">
+          <div className="flex justify-start mb-4">
             <WideButton
               disabled={checkPassword.length > 0 ? false : true}
               content={'تأكيد'}
@@ -123,7 +160,7 @@ const SignUp = () => {
           </div>
         </div>
 
-        <div className="">
+        <div>
           <img draggable="false" src={SingupVector} alt="login vector" />
         </div>
       </div>
