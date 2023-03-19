409ae602e2575ef6655a0884e03fccd42be598eb
Karim.basher <kareem.basher@gmail.com>
2023-03-19 22:16:37 +0200

    WelcomeLogo image is now not draggable.

--- src/components/common/WelcomeLogo.jsx
+++ src/components/common/WelcomeLogo.jsx
@@ -4,7 +4,7 @@ const WelcomeLogo = () => {
   return (
     <div>
       <div className="flex h-[25vh] items-center justify-center">
-        <img src={Logo} className="mx-5 w-[44px]" alt="Logo" />
+        <img src={Logo} draggable="false" className="mx-5 w-[44px]" alt="Logo" />
         <span className="text-[24px]">مرحبا بك في تطبيق أشفينى </span>
       </div>
     </div>
