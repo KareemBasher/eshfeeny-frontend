a44f37748cbf2b84bb6a491ff2b63f12959ed90a
Karim.basher <kareem.basher@gmail.com>
2023-03-20 19:39:54 +0200

    Removed button.

--- src/components/pages/Prescription.jsx
+++ src/components/pages/Prescription.jsx
@@ -15,14 +15,8 @@ const Prescription = ({ loggedInUser }) => {
           <p className="text-right text-[26px]">الروشتة</p>
         </div>
 
-        <div className="cursor-pointer mt-20">
-          <ImageDroper title="أضف صورة الروشتة" />
-        </div>
-
         <div>
-          <button className="text-[24px] py-2 my-2 text-[#F5F5F5] w-[472px] bg-[#0583F2] rounded-[10px] flex justify-center">
-            أعرف أقرب صيدلية
-          </button>
+          <ImageDroper title="أضف صورة الروشتة" buttonTitle={'اعرف اقرب صيدلية'} />
         </div>
       </div>
     </>
