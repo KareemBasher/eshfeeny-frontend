5bb03d45e5f3f126256cb7ec59263d23b995d5de
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-22 22:22:01 +0200

    added UserNavigation component ,images and  link to the companies page

--- src/components/pages/insurance/InsuranceCompanies.jsx
+++ src/components/pages/insurance/InsuranceCompanies.jsx
@@ -2,27 +2,51 @@ import React from 'react'
 import UserNavigation from '../../common/UserNavigation'
 import Image1 from '../../../assets/insuranceCompanies/Image1.png'
 import Image2 from '../../../assets/insuranceCompanies/Image2.png'
+import Arrow from '../../../assets/common/Arrow.svg'
 import { Link } from 'react-router-dom'
 
 const InsuranceCompanies = ({ loggedInUser }) => {
-  const images = [{ url: Image1 }, { url: Image2 }]
+  const images = [
+      { url: Image1, params: 'ميت لايف' },
+      { url: Image2, params: 'مصر هيلث كير' }
+  ]
   return (
     <div>
       <div>
         <UserNavigation loggedInUser={loggedInUser} />
       </div>
-
-      <div className="flex m-5">
-        {images.map(({ url, index }) => (
-          <div
-            className="w-[233px] h-[310px] border rounded-[10px] flex justify-center items-center m-5"
-            key={index}
-          >
-            <Link>
-              <img className="items-center" src={url} alt="inInsuranceCompanies" />
+      <div className="flex flex-col text-right m-14">
+        <div>
+          <div className="flex justify-start mb-5">
+            <Link to="/home" className="hover:underline text-lightBlue">
+              الرئيسية
             </Link>
+
+            <img src={Arrow} draggable="false" className="mx-2" alt="Arrow" />
+            <p className="text-[#1F1F1F]">البديل</p>
           </div>
-        ))}
+        </div>
+        <div className="mb-5">
+          <p className="text-[26px]">أختر شركة التأمين</p>
+        </div>
+
+        <div className="flex ">
+          {images.map(({ url, index, params }) => (
+            <div
+              className="w-[233px] h-[310px] border rounded-[10px] flex justify-center items-center ml-5"
+              key={index}
+            >
+              <Link to={`/insuranceCompanies/${params}`}>
+                <img
+                  draggable="false"
+                  className="items-center"
+                  src={url}
+                  alt="inInsuranceCompanies"
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
