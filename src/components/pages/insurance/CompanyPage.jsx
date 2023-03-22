9498d5de1c8d5cf137eaa4ad81b0a2290de516b6
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-22 22:51:44 +0200

    imported UserNavigation.jsx

--- src/components/pages/insurance/CompanyPage.jsx
+++ src/components/pages/insurance/CompanyPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import UserNavigation from '../../common/UserNavigation'
 
-const CompanyPage = () => {
-  const {company} = useParams()
-  return <div>{company}</div>
+const CompanyPage = ({ loggedInUser }) => {
+  const { company } = useParams()
+  return (
+    <div>
+      <UserNavigation loggedInUser={loggedInUser} />
+    </div>
+  )
 }
 
 export default CompanyPage
