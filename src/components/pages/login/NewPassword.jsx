dd81aa92b3f0c7d835f4b0272d43e8214a0a8a94
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-23 20:15:15 +0200

    imported CredentialsInput in NewPassword.jsx and created 3 useState newPassword, checkPassword, error

--- src/components/pages/login/NewPassword.jsx
+++ src/components/pages/login/NewPassword.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WelcomeLogo from '../../common/WelcomeLogo'
+import CredentialsInput from '../../common/CredentialsInput'
 const NewPassword = () => {
+  const [newPassword, setNewPassword] = useState('')
+  const [checkPassword, setCheckPassword] = useState('')
+  const [error, setError] = useState(false)
+
+  const handleInput = (value, name) => {
+    if (name === 'newPassword') setNewPassword(value)
+    else if (name === 'checkPassword') setCheckPassword(value)
+  }
   return (
     <div>
-      <WelcomeLogo />
+      <div>
+        <WelcomeLogo />
+      </div>
+      <div>
+        <div>
+          <CredentialsInput
+            name={'newPassword'}
+            placeHolder={'البريد الاكتروني'}
+            type={'password'}
+            value={newPassword}
+            handleInput={handleInput}
+          />
+        </div>
+        <div>
+          <CredentialsInput
+            name={'checkPassword'}
+            placeHolder={'البريد الاكتروني'}
+            type={'password'}
+            value={checkPassword}
+            handleInput={handleInput}
+          />
+        </div>
+      </div>
     </div>
   )
 }
