a6d3f73227a4e378e3e7c879f2fbc6ac0f01a3c8
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-23 20:07:09 +0200

    imported WelcomeLogo component in NewPassword

--- src/components/common/NewPassword.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewPassword = () => {
-  return (
-    <div>NewPassword</div>
-  )
-}
-
-export default NewPassword
---
+++ src/components/pages/login/NewPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WelcomeLogo from '../../common/WelcomeLogo'
+const NewPassword = () => {
+  return (
+    <div>
+      <WelcomeLogo />
+    </div>
+  )
+}
+
+export default NewPassword
