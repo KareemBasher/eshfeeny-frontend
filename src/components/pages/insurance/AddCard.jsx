22f05a37f2c7bbe6c537428d48c3ba0d602ab7b7
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-03-24 21:07:19 +0200

    handel input validations

--- src/components/pages/insurance/AddCard.jsx
+++ src/components/pages/insurance/AddCard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserNavigation from '../../common/UserNavigation'
 import CredentialsInput from '../../common/CredentialsInput'
 
 const AddCard = () => {
+  const [cardNumber, setCardNumber] = useState('')
+  const [userName, setUserName] = useState('')
+  const [error, setError] = useState('')
+
+  const handleInput = (value, name) => {
+    if (name === 'cardNumber') setCardNumber(value)
+    else if (name === 'userName') setUserName(value)
+  }
+  const inputValidation = () => {
+    const errorObj = {}
+    if (cardNumber.length === 0) errorObj.cardNumberLength = true
+    else errorObj.cardNumberLength = false
+
+    if (userName.length === 0) errorObj.userNameLength = true
+    else errorObj.userNameLength = false
+    setError(errorObj)
+    if (errorObj.cardNumberLength || errorObj.userNameLength) return false
+    else return true
+  }
+
+  
   return (
     <div>
       <div>
@@ -10,11 +31,26 @@ const AddCard = () => {
       </div>
 
       <div>
+        <div>{/* title */}</div>
         <div>
-          {/* title */}
+          <p>رقم الكارت</p>
+          <CredentialsInput
+            name={'cardNumber'}
+            placeHolder={'أدخل رقم الكارت'}
+            type={'text'}
+            value={cardNumber}
+            handleInput={handleInput}
+          />
         </div>
         <div>
-
+          <p>أسم حامل الكارت</p>
+          <CredentialsInput
+            name={'userName'}
+            placeHolder={'أدخل الأسم'}
+            type={'text'}
+            value={userName}
+            handleInput={handleInput}
+          />
         </div>
       </div>
     </div>
