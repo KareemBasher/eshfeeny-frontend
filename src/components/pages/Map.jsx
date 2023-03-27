f9c1c50936541343277d705399a7884bdeb0e152
Karim.basher <kareem.basher@gmail.com>
2023-03-27 21:56:00 +0200

    Added navigation and page empty component.

--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -1,29 +1,49 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+/*        Assets         */
+import LocationPageEmpty from '../../assets/common/LocationPageEmpty.svg'
+/*    Components    */
+import PageEmpty from '../common/PageEmpty'
+import UserNavigation from '../common/UserNavigation'
 
-const Map = () => {
+const Map = ({ loggedInUser }) => {
+  const { id } = useParams()
   const gcpKey = import.meta.env.VITE_GOOGLE_CLOUD_API_KEY
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: gcpKey
   })
 
-  if (!isLoaded) return 'Loading...'
+  if (!isLoaded || !id)
+    return (
+      <>
+        <UserNavigation loggedInUser={loggedInUser} />
+        <PageEmpty
+          onGetTitle={'اقرب صيدلية'}
+          onGetText="لم يتم العثور علي منتجاتك"
+          onGetLogo={LocationPageEmpty}
+        />
+      </>
+    )
   30.591012255008604, 32.267342084616594
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="h-1/2 w-1/2">
-        <GoogleMap
-          options={{
-            styles: [
-              { elementType: 'labels', featureType: 'poi', stylers: [{ visibility: 'off' }] }
-            ]
-          }}
-          zoom={18}
-          center={{ lat: 30.591012255008604, lng: 32.267342084616594 }}
-          mapContainerStyle={{ height: '100%' }}
-        >
-          <Marker position={{ lat: 30.591012255008604, lng: 32.267342084616594 }} />
-        </GoogleMap>
+    <div>
+      <UserNavigation loggedInUser={loggedInUser} />
+      <div className="h-[75vh] flex justify-center items-center">
+        <div className="h-full w-3/4 mt-20">
+          <GoogleMap
+            options={{
+              styles: [
+                { elementType: 'labels', featureType: 'poi', stylers: [{ visibility: 'off' }] }
+              ]
+            }}
+            zoom={18}
+            center={{ lat: 30.591012255008604, lng: 32.267342084616594 }}
+            mapContainerStyle={{ height: '100%' }}
+          >
+            <Marker position={{ lat: 30.591012255008604, lng: 32.267342084616594 }} />
+          </GoogleMap>
+        </div>
       </div>
     </div>
   )
