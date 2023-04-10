2795c2672050aae2f425e696542833d834efcf04
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-04-10 04:12:35 +0200

    created Error page that contains UserNavigation.jsx and Error.svg

---
+++ src/components/common/ErrorPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import UserNavigation from './UserNavigation'
+import Error from '../../assets/common/Error.svg'
+const ErrorPage = ({ loggedInUser }) => {
+  return (
+    <div>
+      <div>
+        <UserNavigation loggedInUser={loggedInUser} />
+      </div>
+      <div className="flex flex-col items-center my-20">
+        <div>
+          <p className="text-[32px] mb-5">حدث خطأ</p>
+          <p className="text-[28px] mb-10">نعتذر لعدم وجود الصفحة</p>
+        </div>
+        <div>
+          <img src={Error} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage
