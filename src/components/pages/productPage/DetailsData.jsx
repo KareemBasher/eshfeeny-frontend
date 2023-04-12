b134e16594f7b2e481901bcf9e8f8f68c9bbad5f
Karim.basher <kareem.basher@gmail.com>
2023-04-12 16:41:14 +0200

    Changed title and data font sizes.

--- src/components/pages/productPage/DetailsData.jsx
+++ src/components/pages/productPage/DetailsData.jsx
@@ -5,7 +5,7 @@ import Bullet from '../../../assets/common/ListBullet.svg'
 const DetailsData = ({ Title, Data }) => {
   return (
     <ul className="text-[20px] mb-12 ">
-      <p className="text-[28px] mb-3">{Title}</p>
+      <p className="text-[25px] mb-3">{Title}</p>
       {Data &&
         Data.map((data) => (
           <div key={`${data}1`} className="flex items-start pt-3">
@@ -16,7 +16,9 @@ const DetailsData = ({ Title, Data }) => {
               className="ml-4 pt-3"
               alt="BulletPoint"
             />
-            <li key={`${data}3`}>{data}</li>
+            <li className="text-[20px]" key={`${data}3`}>
+              {data}
+            </li>
           </div>
         ))}
     </ul>
