b9934b3b58d0637dee8232c4ecad9e7e23cfa9da
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-04-15 03:49:13 +0200

    created PharmcyFavoutites renders PageEmpty if items.length is zero else renders favourites items

---
+++ src/components/pages/pharmacy/PharmacyFavourits.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import PharmacyNavigation from '../../common/PharmacyNavigation'
+import PageEmpty from '../../common/PageEmpty'
+import PharmacyFavouritesContent from './PharmacyFavouritesContent'
+import HeartLight from '../../../assets/common/HeartLight.svg'
+
+const PharmacyFavourits = ({ loggedInUser }) => {
+  const [items, setItems] = useState([])
+
+  return (
+    <div>
+      <div>
+        <PharmacyNavigation />
+      </div>
+      <div>
+        <div>
+          {items.length ? (
+            <div>
+              <PharmacyFavouritesContent
+                onGetItems={items}
+                onGetTitle="المنتجات المفضلة"
+                loggedInUser={loggedInUser}
+              />
+            </div>
+          ) : (
+            <PageEmpty
+              onGetLogo={HeartLight}
+              onGetText="لا توجد أي منتجات مفضلة لديك"
+              onGetTitle="المنتجات المفضلة"
+              onGetPath="/pharmacy"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PharmacyFavourits
