9b6b32aa3372c739fa95074e38aa1053d54f94c0
Karim.basher <kareem.basher@gmail.com>
2023-04-18 00:24:50 +0200

    Added favorites and cart items methods.

--- src/utils/pharmaciesAPI.js
+++ src/utils/pharmaciesAPI.js
@@ -64,3 +64,89 @@ export const getPharmacy = async (id) => {
     console.log(`Could not get pharmacy with id ${id}.`)
   }
 }
+
+// Add a product to favorites
+export const addToFavorites = async (id, productID) => {
+  try {
+    const result = await fetch(apiURL + `/pharmacies/${id}/favorites`, {
+      method: 'PATCH',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ productId: productID })
+    })
+    if (result.status === 200) return result.json()
+  } catch (error) {
+    console.log(`Could not add product with id ${id} to favorites.`)
+  }
+}
+
+// Add a product to cart
+export const addToCart = async (id, productID) => {
+  try {
+    const result = await fetch(apiURL + `/pharmacies/${id}/cart`, {
+      method: 'PATCH',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ productId: productID })
+    })
+    if (result.status === 200) return result.json()
+  } catch (error) {
+    console.log(`Could not add product with id ${id} to cart.`)
+  }
+}
+
+// Remove a product from favourites
+export const removeFromFavorites = async (userID, productID) => {
+  try {
+    const result = await fetch(apiURL + `/pharmacies/${userID}/favorites/${productID}`, {
+      method: 'DELETE',
+      headers: headers
+    })
+    if (result.status === 200) return result.json()
+  } catch (error) {
+    console.log(`Could not remove product.`)
+  }
+}
+
+// Remove a product from cart
+export const removeFromCart = async (userID, productID) => {
+  try {
+    const result = await fetch(apiURL + `/pharmacies/${userID}/cart/${productID}`, {
+      method: 'DELETE',
+      headers: headers
+    })
+    if (result.status === 200) return result.json()
+  } catch (error) {
+    console.log(`Could not remove product.`)
+  }
+}
+
+// Increment the quantity of a product in the cart
+export const incrementQuantity = async (userID, productID) => {
+  try {
+    const result = await fetch(apiURL + `/pharmacies/${userID}/cart/${productID}/1`, {
+      method: 'PATCH',
+      headers: headers
+    })
+    if (result.status === 200) return result.json()
+  } catch (error) {
+    console.log(`Could not increment quantity.`)
+  }
+}
+
+// Decrement the quantity of a product in the cart
+export const decrementQuantity = async (userID, productID) => {
+  try {
+    const result = await fetch(apiURL + `/pharmacies/${userID}/cart/${productID}/-1`, {
+      method: 'PATCH',
+      headers: headers
+    })
+    if (result.status === 200) return result.json()
+  } catch (error) {
+    console.log(`Could not decrement quantity.`)
+  }
+}
