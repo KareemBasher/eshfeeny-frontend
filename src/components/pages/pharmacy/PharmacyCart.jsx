4b6ab4f57a69273d2f2f54872acdc7b0def35bef
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-04-25 11:30:51 +0200

    called removeFromCart to Remove product and added PageEmpty For guest

--- src/components/pages/pharmacy/PharmacyCart.jsx
+++ src/components/pages/pharmacy/PharmacyCart.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 /*    components    */
 import PageEmpty from '../../common/PageEmpty'
 import PharmacyNavigation from '../../common/PharmacyNavigation'
 import PharmacyCartContent from './PharmacyCartContent'
 /*    Icons    */
 import CartLight from '../../../assets/common/CartLight.svg'
+import GuestLogo from '../../../assets/common/AlternativeLogo.svg'
+/*    API    */
+import * as PharmacyAPI from '../../../utils/pharmaciesAPI'
 
 const PharmacyCart = ({ loggedInUser, logout }) => {
   const [items, setItems] = useState([])
+
+  // useEffect(() => {
+  //   const updateItems = async () => {
+  //     const result = await PharmacyAPI.getCartProducts(loggedInUser)
+  //     setItems(result?.cart ? result.cart : [])
+  //   }
+  //   updateItems()
+  // }, [])
+
+  const removeFromCart = (ID, productID) => {
+    PharmacyAPI.removeFromCart(ID, productID)
+    setItems(items.filter(({ product }) => product._id !== productID))
+  }
 
   return (
     <div>
@@ -16,18 +32,36 @@ const PharmacyCart = ({ loggedInUser, logout }) => {
       </div>
 
       <div>
-        {items.length ? (
+        {loggedInUser === '6439bd5e1c12d023717e2be5' ? (
           <div>
-            <PharmacyCartContent onGetItems={items} OnGetTitle="سلة التسوق" />
+            <PageEmpty
+              onGetTitle="سلة التسوق"
+              onGetLogo={GuestLogo}
+              onGetText1="أنت الان في وضع الضيف"
+              onGetText2="الرجاء تسجيل الدخول للاستمتاع بالمميزات"
+              onGetButtonText="تسجل الدخول"
+              onGetPath="./login/pahrmacy"
+            />
           </div>
         ) : (
-          <PageEmpty
-            onGetTitle="سلة التسوق"
-            onGetLogo={CartLight}
-            onGetText1="عربة التسوق فارغة "
-            onGetButtonText="اذهب للتسوق الان"
-            onGetPath="/pharmacy"
-          />
+          <div>
+            {items.length ? (
+              <PharmacyCartContent
+                onGetItems={items}
+                OnGetTitle="سلة التسوق"
+                loggedInUser={loggedInUser}
+                onRemoveItem={removeFromCart}
+              />
+            ) : (
+              <PageEmpty
+                onGetTitle="سلة التسوق"
+                onGetLogo={CartLight}
+                onGetText1="عربة التسوق فارغة "
+                onGetButtonText="اذهب للتسوق الان"
+                onGetPath="/home"
+              />
+            )}
+          </div>
         )}
       </div>
     </div>
