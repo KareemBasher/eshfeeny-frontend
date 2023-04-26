9632873223ce49c0afedabf5b5ed062cf6b954b6
Karim.basher <kareem.basher@gmail.com>
2023-04-26 23:43:25 +0200

    Added street names to map.

--- src/components/pages/map/mapStyle.js
+++ src/components/pages/map/mapStyle.js
@@ -1,18 +1,6 @@
 const style = [
   {
     featureType: 'all',
-<<<<<<< HEAD
-=======
-    elementType: 'labels.text',
-    stylers: [
-      {
-        visibility: 'off'
-      }
-    ]
-  },
-  {
-    featureType: 'all',
->>>>>>> b9a05a47d7ecd527aaa349c9e14283c60d60bcdc
     elementType: 'labels.icon',
     stylers: [
       {
