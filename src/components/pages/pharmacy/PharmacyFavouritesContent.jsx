a1456a4d800f370ba816ccc0fbad4335aef57de4
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-04-27 13:02:33 +0200

    created PharmacyFavouritesContent.jsx that contain PharmacyProductContainer.jsx

--- src/components/pages/pharmacy/PharmacyFavouritesContent.jsx
+++ src/components/pages/pharmacy/PharmacyFavouritesContent.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+/*     Components      */
+import PharmacyProductContainer from '../../common/PharmacyProductContainer'
 
-const PharmacyFavouritesContent = ({ onGetTitle, onGetItems, loggedInUser }) => {
+const PharmacyFavouritesContent = ({ onGetTitle, onGetItems, loggedInUser, favorites }) => {``
   return (
-    <div>
-      <div className="text-right text-[28px] my-10 mr-32 2xl:mr-52">{onGetTitle}</div>
+    <div className="mr-32 2xl:mr-52">
+      <div className="text-right text-[28px] my-10">{onGetTitle}</div>
+      <ol className="flex flex-wrap justify-start -mr-2">
+        {onGetItems.map((product) => (
+          <li key={product._id}>
+            <PharmacyProductContainer
+              onGetProduct={product}
+              loggedInUser={loggedInUser}
+              favorites={favorites}
+            />
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
