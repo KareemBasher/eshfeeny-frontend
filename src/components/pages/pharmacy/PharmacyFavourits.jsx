add90a5c4f65b6c6d8d4137a1962c113e28d8c02
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-04-27 13:03:39 +0200

    created PharmacyFavourits.jsx the containes how the Pharmacy Favourites page should look

--- src/components/pages/pharmacy/PharmacyFavourits.jsx
+++ src/components/pages/pharmacy/PharmacyFavourits.jsx
@@ -4,39 +4,48 @@ import PharmacyNavigation from '../../common/PharmacyNavigation'
 import PageEmpty from '../../common/PageEmpty'
 import PharmacyFavouritesContent from './PharmacyFavouritesContent'
 /*    Icons    */
+import GuestLogo from '../../../assets/common/AlternativeLogo.svg'
 import HeartLight from '../../../assets/common/HeartLight.svg'
 
 const PharmacyFavourits = ({ loggedInUser, logout }) => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([''])
+  const [favoritesId, setFavouriteId] = useState([])
 
   return (
-    <div>
-      <div>
-        <PharmacyNavigation loggedInUser={loggedInUser} logout={logout} />
-      </div>
-
-      <div>
+    <>
+      <PharmacyNavigation loggedInUser={loggedInUser} logout={logout} />
+      {loggedInUser === '6439bd5e1c12d023717e2be5' ? (
+        <div>
+          <PageEmpty
+            onGetTitle="المنتجات المفضلة"
+            onGetLogo={GuestLogo}
+            onGetText1="أنت الان في وضع الضيف"
+            onGetText2="الرجاء تسجيل الدخول للاستمتاع بالمميزات"
+            onGetButtonText="تسجيل الدخول"
+            onGetPath="./login/user"
+          />
+        </div>
+      ) : (
         <div>
           {items.length ? (
-            <div>
-              <PharmacyFavouritesContent
-                onGetItems={items}
-                onGetTitle="المنتجات المفضلة"
-                loggedInUser={loggedInUser}
-              />
-            </div>
+            <PharmacyFavouritesContent
+              onGetTitle="المنتجات المفضلة"
+              onGetItems={items}
+              loggedInUser={loggedInUser}
+              favorites={favoritesId}
+            />
           ) : (
             <PageEmpty
+              onGetTitle="المنتجات المفضلة"
               onGetLogo={HeartLight}
               onGetText1="لا توجد أي منتجات مفضلة لديك"
               onGetButtonText="اذهب للتسوق الان"
-              onGetTitle="المنتجات المفضلة"
-              onGetPath="/pharmacy"
+              onGetPath="/home"
             />
           )}
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
