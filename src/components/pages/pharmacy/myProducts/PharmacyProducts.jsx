2ebbd19a98f12085ba2bf3ce5a89edc5d1d0b639
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-05-03 01:42:31 +0300

    called getPharmacyProducts in PharmacyProduct.jsx

--- src/components/pages/pharmacy/myProducts/PharmacyProducts.jsx
+++ src/components/pages/pharmacy/myProducts/PharmacyProducts.jsx
@@ -1,21 +1,22 @@
 /*     HOOKS      */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 /*     components      */
 import PharmacyNavigation from '../../../common/PharmacyNavigation'
 import PharmacyProductContent from './PharmacyProductContent'
 import Img from './img.png'
+import * as ProductAPI from '../../../../utils/productsAPI'
 
 const PharmacyProducts = ({ loggedInUser, logout }) => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      img: Img,
-      name: 'سنسوداين | معجون اسنان ترميم وحماية | 75مل',
-      price: 70,
-      quantity: 4
-    }
-  ])
+  const [items, setItems] = useState([])
 
+  useEffect(() => {
+    const updateItems = async () => {
+      const result = await ProductAPI.getPharmacyProducts(loggedInUser)
+      setItems(result ? result : [])
+      console.log(result)
+    }
+    updateItems()
+  }, [])
   return (
     <div>
       <PharmacyNavigation loggedInUser={loggedInUser} logout={logout} />
