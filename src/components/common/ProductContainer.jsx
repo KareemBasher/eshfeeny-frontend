38355c170d72eac301f9283e10118f940f98efd5
Karim.basher <kareem.basher@gmail.com>
2023-05-04 20:33:22 +0200

    Changed rounded corners to 10px.

--- src/components/common/ProductContainer.jsx
+++ src/components/common/ProductContainer.jsx
@@ -35,7 +35,7 @@ const ProductContainer = ({ onGetProduct, loggedInUser, favorites }) => {
   }
 
   return (
-    <div className="flex flex-col justify-between rounded-lg hover:shadow-xl transition-all duration-300 shadow-sm w-[233px] h-[340px] border-[#E7E7E7] border-[0.8px] mx-2 my-4">
+    <div className="flex flex-col justify-between rounded-[10px] hover:shadow-xl transition-all duration-300 shadow-sm w-[233px] h-[340px] border-[#E7E7E7] border-[0.8px] mx-2 my-4">
       <button
         className="w-[22px] box-border mr-3 mt-5"
         onClick={() => (itemInFavorites ? handleRemove() : handleAdd())}
