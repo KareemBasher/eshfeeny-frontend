36134eb0c8bcbefc08167f88f67db412f5f26378
Abdalla <bodi.tarek.bt@gmail.com>
2023-05-05 23:26:47 +0300

    Added pagination to my products in pharmacy

--- src/components/pages/pharmacy/myProducts/PharmacyProductContent.jsx
+++ src/components/pages/pharmacy/myProducts/PharmacyProductContent.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 /*     components     */
 import MyProductContainer from './MyProductContainer'
+import Pagination from './Pagination'
 
 const PharmacyProductContent = ({ items }) => {
+  let result = 9
+  if (innerWidth === 1920) {
+    result = 15
+  }
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [productsPerPage] = useState(result)
+
+  const lastProductIndex = currentPage * productsPerPage
+  const firstProductIndex = lastProductIndex - productsPerPage
+  const currentProducts = items.slice(firstProductIndex, lastProductIndex)
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className="flex flex-wrap">
-      {items.map((item) => (
+      {currentProducts.map((item) => (
         <MyProductContainer product={item} key={item._id} />
       ))}
+      <Pagination
+        totalProducts={items?.length}
+        productsPerPage={productsPerPage}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </div>
   )
 }
