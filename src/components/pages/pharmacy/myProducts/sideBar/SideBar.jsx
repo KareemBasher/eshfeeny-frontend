857d43b7bd3679fcf64606171a1bfa9f0e55c554
Karim.basher <kareem.basher@gmail.com>
2023-05-07 21:37:23 +0200

    Added CategorySelector component.

--- src/components/pages/pharmacy/myProducts/sideBar/SideBar.jsx
+++ src/components/pages/pharmacy/myProducts/sideBar/SideBar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import TypeSelector from './TypeSelector'
+import CategorySelector from './CategorySelector'
+import FilterIcon from '../../../../../assets/pharmacyProducts/FilterIcon.svg'
 
-const SideBar = ({ selectedType, changeSelectedType }) => {
+const SideBar = ({
+  selectedType,
+  selectedCategories,
+  changeSelectedType,
+  changeSelectedCategories
+}) => {
   return (
     <div className="mt-4 ml-1">
       <TypeSelector changeSelectedType={changeSelectedType} selectedType={selectedType} />
+      {selectedType !== 'كل المنتجات' && (
+        <>
+          <div className="flex mt-5 px-3">
+            <img draggable="false" src={FilterIcon} alt="Filter" />
+            <p className="text-[22px] mx-3">تصفية</p>
+          </div>
+          <CategorySelector
+            selectedCategories={selectedCategories}
+            changeSelectedCategories={changeSelectedCategories}
+            selectedType={selectedType}
+          />
+        </>
+      )}
     </div>
   )
 }
