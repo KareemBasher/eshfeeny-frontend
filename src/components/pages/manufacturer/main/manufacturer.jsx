599d83fc4eeaa9cf6228267be529d3646a0b4c13
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-06-13 20:52:34 +0300

    resolved confliects

--- src/components/pages/manufacturer/main/manufacturer.jsx
+++ src/components/pages/manufacturer/main/manufacturer.jsx
@@ -2,30 +2,36 @@ import React from 'react'
 /*       components       */
 import ManufacturerNavigation from '../../../common/ManufacturerNavigation'
 import ManufacturerCategoryItems from '../ManufacturerCategoryItems'
+import Chart from '../chart/Chart'
 
 const manufacturer = ({ loggedInUser, logout }) => {
   return (
     <div>
       <ManufacturerNavigation loggedInUser={loggedInUser} logout={logout} />
+
+      <div className="mx-32 2xl:mx-52 my-10">
+        <Chart />
+      </div>
+
       <ManufacturerCategoryItems
         onGetTitle="الأكثر مبيعا"
-        onGetType="الحمايه من الفيروسات"
-        onGetCategory="تقوية المناعة"
+        onGetType={false}
+        onGetCategory={['الحموضة وسوء الهضم', 'الفيتامينات و المكملات الغذائية']}
         onGetContainer="one"
         loggedInUser={loggedInUser}
       />
       <ManufacturerCategoryItems
         onGetTitle="العروض الأسبوعية"
-        onGetType="الأدوية"
-        onGetCategory="الكحة"
+        onGetType={null}
+        onGetCategory={['المغص', 'مسكنات']}
         onGetContainer="two"
         loggedInUser={loggedInUser}
       />
       <ManufacturerCategoryItems
         onGetTitle="قرب لانتهاء الصلاحية"
-        onGetType="الأدوية"
-        onGetCategory="مسكنات"
-        onGetContainer="two"
+        onGetType={null}
+        onGetCategory={['المغص', 'امساك']}
+        onGetContainer="tree"
         loggedInUser={loggedInUser}
       />
     </div>
