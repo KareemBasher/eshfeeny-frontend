d7f6c013fabeb0c1a8cea40e38afddd8d3bc533d
Karim.basher <kareem.basher@gmail.com>
2023-06-13 16:32:31 +0200

    Added chart component to main manufacturer page.

--- src/components/pages/manufacturer/main/manufacturer.jsx
+++ src/components/pages/manufacturer/main/manufacturer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 /*       components       */
 import ManufacturerNavigation from '../../../common/ManufacturerNavigation'
+import Chart from '../chart/Chart'
 
 const manufacturer = ({ loggedInUser, logout }) => {
   return (
     <div>
       <ManufacturerNavigation loggedInUser={loggedInUser} logout={logout} />
+
+      <div className="mx-32 2xl:mx-52 my-10">
+        <Chart />
+      </div>
     </div>
   )
 }
