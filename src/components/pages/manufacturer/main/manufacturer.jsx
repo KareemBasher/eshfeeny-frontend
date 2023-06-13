94365726cb4d16cd7c78cdd9fb58b8618a3aa766
Mohamed Ashraf <mohamed.ashraf.elrokh@gmail.com>
2023-06-13 19:42:11 +0300

    imported ManufacturerCategoryItems tekes onGetTitle,   onGetType,   onGetCategory,   onGetContainer,   loggedInUser as a props

--- src/components/pages/manufacturer/main/manufacturer.jsx
+++ src/components/pages/manufacturer/main/manufacturer.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 /*       components       */
 import ManufacturerNavigation from '../../../common/ManufacturerNavigation'
+import ManufacturerCategoryItems from '../ManufacturerCategoryItems'
 
 const manufacturer = ({ loggedInUser, logout }) => {
   return (
     <div>
       <ManufacturerNavigation loggedInUser={loggedInUser} logout={logout} />
+      <ManufacturerCategoryItems
+        onGetTitle="الأكثر مبيعا"
+        onGetType="الحمايه من الفيروسات"
+        onGetCategory="تقوية المناعة"
+        onGetContainer="one"
+        loggedInUser={loggedInUser}
+      />
+      <ManufacturerCategoryItems
+        onGetTitle="العروض الأسبوعية"
+        onGetType="الأدوية"
+        onGetCategory="الكحة"
+        onGetContainer="two"
+        loggedInUser={loggedInUser}
+      />
+      <ManufacturerCategoryItems
+        onGetTitle="قرب لانتهاء الصلاحية"
+        onGetType="الأدوية"
+        onGetCategory="مسكنات"
+        onGetContainer="two"
+        loggedInUser={loggedInUser}
+      />
     </div>
   )
 }
