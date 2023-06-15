4e89b17efeb11c874594400809b3c2b598efaf00
Abdalla <bodi.tarek.bt@gmail.com>
2023-06-15 12:31:45 +0300

    Fixed promise problem, added orders to the table

--- src/components/pages/manufacturer/orders/DelayedOrders.jsx
+++ src/components/pages/manufacturer/orders/DelayedOrders.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 /*        Components       */
 import ManufacturerNavigation from '../../../common/ManufacturerNavigation'
 import Orders from './Orders'
+/*    API      */
+import { getDelayedOrders } from '../../../../utils/manufacturersAPI'
 
 const DelayedOrders = ({ loggedInUser, logout }) => {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    const getCurrentOrders = async () => {
+      setOrders(await getDelayedOrders(loggedInUser))
+    }
+    getCurrentOrders()
+  }, [])
   return (
     <div>
       <ManufacturerNavigation loggedInUser={loggedInUser} logout={logout} />
-      <Orders onGetTitle="الطلبات المؤجلة" />
+      <Orders onGetTitle="الطلبات المؤجلة" orders={orders.orders} />
     </div>
   )
 }
---
+++ src/components/pages/manufacturer/orders/RowContent.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const RowContent = ({ data }) => {
+  return (
+    <>
+      {/*  Image and Name   */}
+      <td className="flex h-[103px]">
+        <div className="flex p-2 min-w-fit">
+          <img src={data.productImage} />
+        </div>
+        <p className="flex justify-center items-center w-full px-5">
+          {data.productName} {data.productVolume ? ` | ${data.productVolume}` : ''}
+          {data.productAmount ? ` | ${data.productAmount}` : ''}
+        </p>
+      </td>
+      {/*     Quantity     */}
+      <td>{data.quantity} عبوة</td>
+      {/*  Total Price   */}
+      <td>{data.totalPrice} جنيه</td>
+      {/*  Pharmacy Name   */}
+      <td>{data.pharmacyName}</td>
+      {/*  Phone Number   */}
+      <td>{data.phoneNumber}</td>
+      {/*  Address   */}
+      <td>{data.pharmacyAdress}</td>
+      {/*  Order Status   */}
+      <td>:</td>
+    </>
+  )
+}
+
+export default RowContent
